704fc34e6aa5d356126803ef8d293120c3aab321
hxstwo <stefan.wolf@th-owl.de>
2022-05-30 16:37:20 +0200

    First Commit

---
+++ VorlesungHallo/src/hallo/HalloWelt.java
@@ -0,0 +1,13 @@
+package hallo;
+
+public class HalloWelt {
+
+	public HalloWelt() {
+		System.out.println("Hallo Welt");
+	}
+
+	public static void main(String[] args) {
+		new HalloWelt();
+	}
+
+}
